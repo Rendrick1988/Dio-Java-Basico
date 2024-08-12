9f2770e8a24b661c7473eeefd7af4544dae69bea
Oliveira <Patriota1988@protonmail.com>
2024-08-12 20:36:33 -0300

    Projetos de Tipos  de Variaveis

---
+++ tipos-variaveis/src/TiposdeOperadores.java
@@ -0,0 +1,21 @@
+public class TiposdeOperadores {
+    public static void main(String[] args) throws Exception {
+      String concatenacao = "?";
+
+       concatenacao = "1"+1+1+1+"1";
+
+       System.out.println(concatenacao);
+
+       concatenacao = 1+1+1+"1"+1;
+
+       System.out.println(concatenacao);
+
+       concatenacao = 1+1+1+"1"+"1";
+
+       System.out.println(concatenacao);
+
+
+
+    }
+
+}
